08baf3d78981b092df2e52260a56046e3fb6b2f8
Christian Deese <s238007@students.aie.edu>
2023-09-13 11:38:32 -0500

    added start, update, and end functions and organized functions/code

--- HelloDungeon/Game.cs
+++ HelloDungeon/Game.cs
@@ -10,22 +10,118 @@ namespace HelloDungeon
 {
     class Game
     {
+        //game variables
         struct Character
         {
-           public string name;
-           public float health;
-           public float strength;
-           public float baseDamage;
-           public float dexterity;
+            public string name;
+            public float health;
+            public float strength;
+            public float baseDamage;
+            public float dexterity;
+            public Weapon equippedWeapon;
+        }
+        struct Weapon
+        {
+            public string name;
+            public float damage;
+            public string effectName;
+            public int effectDamage;
+        }
+        bool gameOver;
+        bool playerAlive;
+        int stageNumber;
+        Character player;
+        Character enemy;
+
+
+        //unfinished functions
+
+        void selectClass()
+        {
+            int selectedClass = 0;
+            player.name = getText("Enter player name");
+            while (selectedClass == 0)
+            {
+                selectedClass = getInput("Select your class.", "Basketball Player", "Business man", "Hobo", "", "");
+
+
+                //give class with selectClass function
+
+                printStats(player);
+
+                //have a starting weapon and choose to drop it
+
+            }
+            //change stats here with if
+            return;
         }
 
-        //game variables
+        void Stage(int stageNumber)
+        {
+            if (stageNumber == 1)
+            {
+                createEnemy("ghoul");
+                Battle(ref enemy);
+            }
+            else if (stageNumber == 2)
+            {
+                createEnemy("bear");
+                Battle(ref enemy);
+            }
+            else if (stageNumber == 3)
+            {
+                createEnemy("dragon");
+                Battle(ref enemy);
+            }
+            else
+            {
+                quitGame();
+            }
+
+        }
+
+        Character quickAttack(Character initiator, ref Character receiver)
+        {
+            receiver.health -= initiator.baseDamage * (initiator.strength + 1);
+            return receiver;
+        }
+
+        void Battle(ref Character enemy)
+        {
+            printStats(player);
+        }
+
+        void weaponDamage()
+        {
+            if (player.equippedWeapon == "Sword")
+            {
+                //Bleeding
+            }
+            else if (player.equippedWeapon == "Needle")
+            {
+                //Poison
+            }
+            else if (player.equippedWeapon == "Long Stick")
+            {
+                //Blunt, no extra damage
+            }
+            else
+            {
+                //unarmed
+            }
+            return;
+        }
+        void proceed()
+        {
+            Console.WriteLine("Press any key to proceed.");
+            Console.ReadKey(true);
+            Console.Clear();
+        }
+
+        //   void lvlUP(string character)
 
 
-        //player stats
-
-
-        //enemy stats
+        //game functions
 
         void printStats(Character print)
         {
@@ -37,23 +133,22 @@ namespace HelloDungeon
             op.health += health;
         }
 
-        //broke
         //option 3-5 are optional and can toggled out by entering "" into the parameter slots for 3-5 when calling the function
         int getInput(string prompt, string option1, string option2, string option3, string option4, string option5)
         {
             string userInput = "";
             int result = 0;
-            while(result != 1 && result != 2 && !(result == 3 && option3 != "") && !(result == 4 && option4 != "") && !(result == 5 && option5 != ""))
+            while (result != 1 && result != 2 && !(result == 3 && option3 != "") && !(result == 4 && option4 != "") && !(result == 5 && option5 != ""))
             {
                 Console.WriteLine(prompt);
                 Console.WriteLine("1. " + option1 + "\n2. " + option2);
-                if(option3 != "")
+                if (option3 != "")
                 {
                     Console.WriteLine("3. " + option3);
-                    if(option4 != "")
+                    if (option4 != "")
                     {
                         Console.WriteLine("4. " + option4);
-                        if(option5 != "")
+                        if (option5 != "")
                         {
                             Console.WriteLine("5. " + option5);
                         }
@@ -62,7 +157,7 @@ namespace HelloDungeon
                 Console.Write("> ");
 
                 userInput = Console.ReadLine();
-                if(userInput != "1" && userInput != "2" && !(userInput == "3" && option3 != "") && !(userInput == "4" && option4 != "") && !(userInput == "5" && option5 != ""))
+                if (userInput != "1" && userInput != "2" && !(userInput == "3" && option3 != "") && !(userInput == "4" && option4 != "") && !(userInput == "5" && option5 != ""))
                 {
                     Console.WriteLine("Invalid Input, to try again, press any key.");
                     userInput = "";
@@ -73,7 +168,7 @@ namespace HelloDungeon
                 {
                     //do nothing
                 }
-                else if(userInput == "1")
+                else if (userInput == "1")
                 {
                     result = 1;
                 }
@@ -83,11 +178,11 @@ namespace HelloDungeon
                 }
                 else if (option3 != "")
                 {
-                    if(userInput == "3")
+                    if (userInput == "3")
                     {
                         result = 3;
                     }
-                    if(option4 != "")
+                    if (option4 != "")
                     {
                         if (userInput == "4")
                         {
@@ -102,27 +197,44 @@ namespace HelloDungeon
                         }
                     }
                 }
-               
+
             }
             Console.Clear();
             return result;
 
         }
 
-        void selectClass(ref Character player)
+        void createEnemy(string enemyName)
         {
-            int selectedClass = 0;
-            while(selectedClass == 0)
+            if (enemyName == "ghoul")
             {
-                selectedClass = getInput("Select your class.", "Basketball Player", "Business man", "Hobo", "", "");
-
-
-                //give class with selectClass function
-
-                printStats(player);
-
+                enemy.name = "Ghoul";
+                enemy.health = 30;
+                enemy.strength = 1;
+                enemy.baseDamage = 5;
+                enemy.dexterity = 0.5f;
             }
-            //change stats here with if
+            else if (enemyName == "bear")
+            {
+                enemy.name = "Bear";
+                enemy.health = 75;
+                enemy.strength = 4;
+                enemy.baseDamage = 4;
+                enemy.dexterity = 3;
+            }
+            else if (enemyName == "dragon")
+            {
+                enemy.name = "Dragon";
+                enemy.health = 200;
+                enemy.strength = 10;
+                enemy.baseDamage = 10;
+                enemy.dexterity = 5;
+            }
+            else
+            {
+                Console.WriteLine("Error, failed enemy creation!");
+                //self destruct
+            }
             return;
         }
 
@@ -138,7 +250,7 @@ namespace HelloDungeon
                 Console.WriteLine();
 
                 //check input
-                if(text != "" && text != null)
+                if (text != "" && text != null)
                 {
                     Console.WriteLine(text);
                     return text;
@@ -155,89 +267,85 @@ namespace HelloDungeon
             return "Bob";
         }
 
-        void createEnemy(ref Character badguy)
+        bool quitGame()
         {
-            if (badguy.name == "Ghoul")
+            int response;
+            if(playerAlive == false)
             {
-                badguy.health = 30;
-                badguy.strength = 1;
-                badguy.baseDamage = 5;
-                badguy.dexterity = 0.5f;
-            }
-            else if (badguy.name == "Bear")
-            {
-                badguy.health = 75;
-                badguy.strength = 4;
-                badguy.baseDamage = 4;
-                badguy.dexterity = 3;
-            }
-            else if (badguy.name == "Dragon")
-            {
-                badguy.health = 200;
-                badguy.strength = 10;
-                badguy.baseDamage = 10;
-                badguy.dexterity = 5;
+                response = getInput("Do you want to play again?", "Restart", "Quit Game", "", "", "");
+                response++;
             }
             else
             {
-                Console.WriteLine("Error, failed enemy creation!");
-                //self destruct
-            }
-            return;
-        }
-
-        void quitGame()
-        {
-
-        }
-
-        void Stage(int stageNumber)
-        {
-            if(stageNumber == 1)
-            {
-                Character ghoul;
-                ghoul = selectClass(ghoul);
-                ghoul = quickAttack(player, ghoul);
-            }
-            else if(stageNumber == 2)
-            {
-                Character bear;
-            }
-            else if(stageNumber == 3)
-            {
-                Character dragon;
+                response = getInput("Do you want to continue?", "Continue", "Restart", "Quit Game", "", "");
             }
             
+            if (response == 1)
+            {
+                return false;
+            }
+            else if (response == 2)
+            {
+                playerAlive = false;
+                return false;
+            }
+            else
+            {
+                return true;
+            }
         }
 
-        void lvlUP(string character)
-        {
 
+
+        //base functions
+
+        //start - called before the first loop and initalizes variables
+        void Start()
+        {
+            gameOver = false;
+            playerAlive = true;
+            stageNumber = 1;
+
+            selectClass();
         }
 
-        Character quickAttack(Character initiator, Character receiver)
+
+        //update - called every time the game loops, like player input, character postions, game logic
+        void Update()
         {
-            receiver.health -= initiator.baseDamage * (initiator.strength + 1);
-            return receiver;
+            while (!gameOver)
+            {
+
+                if (!playerAlive)
+                {
+                    Console.WriteLine("You Died!");
+                    gameOver = quitGame();
+                    continue;
+                }
+                Stage(stageNumber);
+                proceed();
+            }
         }
-        
-        void Battle(Character player, Character enemy)
+
+
+        //end - called after game loop exits
+        //Used to clean up memory or display end game messages
+        void End()
         {
-            printStats(player);
+            Console.WriteLine("Thanks for playing!");
         }
+
 
         public void Run()
         {
-            Character player = selectClass(player);
-            player.name = getText("Enter player name");
-            player.health = 0;
-            heal(player, 50);
-            Console.WriteLine(player.health);
-            int n = 1;
-            Stage(n);
-        
+            Start();
+            while (!gameOver)
+            {
+                Update();
+            }
+            End();
         }
 
-        
+
     }
 }
